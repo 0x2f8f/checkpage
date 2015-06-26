9d043ca5ba26742fc689d66718ef852d37fbcda3
0x2f8f <0x2f8f@gmail.com>
2015-06-27 00:47:59 +0500

    issue-9

---
+++ src/ItBlaster/MainBundle/Admin/ProjectLinkAdmin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace ItBlaster\MainBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class ProjectLinkAdmin extends Admin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+//            ->add('Id')
+            ->add('Link')
+            ->add('Active')
+            ->add('ProjectId')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('Id')
+            ->add('Link')
+            ->add('Active')
+            ->add('ProjectId')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+//            ->add('Id')
+            ->add('Link')
+            ->add('Active')
+            ->add('ProjectId')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('Id')
+            ->add('Link')
+            ->add('Active')
+            ->add('ProjectId')
+        ;
+    }
+}
---
+++ src/ItBlaster/MainBundle/Model/ProjectLink.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ItBlaster\MainBundle\Model;
+
+use ItBlaster\MainBundle\Model\om\BaseProjectLink;
+
+class ProjectLink extends BaseProjectLink
+{
+}
